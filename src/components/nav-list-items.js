618150ce048c50115b742b2f6ecd0c7b591a5ac7
Alyssa Voccia <avoccia@hotmail.com>
2022-03-16 13:10:32 -0400

    Working on navbar and dashboard page

---
+++ src/components/nav-list-items.js
@@ -0,0 +1,60 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout'
+import PeopleIcon from '@mui/icons-material/People'
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import FilePresentIcon from '@mui/icons-material/FilePresent';
+
+import { Link } from 'react-router-dom';
+
+export const mainListItems = (
+  <div>
+    <ListItem button to='/dashboard' component={Link}>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <ListItem button to='/profile' component={Link}>
+      <ListItemIcon>
+        <PersonIcon />
+      </ListItemIcon>
+      <ListItemText primary="Profile" />
+    </ListItem>
+    {/* <ListItem button to='/' onClick={() => auth.signOut()} component={Link}>
+      <ListItemIcon>
+        <LogoutIcon />
+      </ListItemIcon>
+      <ListItemText primary="Logout" />
+    </ListItem> */}
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListItem button to='/team' component={Link}>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Team" />
+    </ListItem>
+    <ListItem button to='/player-data' component={Link}>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Player Data" />
+    </ListItem>
+  </div>
+);
+
+export const highschoolAdminListItems = (
+  <ListItem button to='/hs-link-submissions' component={Link}>
+      <ListItemIcon>
+        <FilePresentIcon />
+      </ListItemIcon>
+    <ListItemText primary="Player Submissions" />
+  </ListItem>
+)
