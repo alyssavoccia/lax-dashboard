4088f9b23fa4fb1c42ee4aef3201555a2c795944
Alyssa Voccia <avoccia@hotmail.com>
2022-03-18 12:23:48 -0400

    Added private route and useAuthStatus custom hook to check for logged in user before trying to render ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { getAuth } from 'firebase/auth';
+import { useAuthStatus } from './hooks/useAuthStatus';
 
 import Box from '@mui/material/Box';
 
 import Navbar from "./components/navbar/Navbar";
+import PrivateRoute from './components/PrivateRoute';
 import Dashboard from "./pages/Dashboard";
 import SignInSignUp from './pages/SignInSignUp';
 
 function App() {
-  const auth = getAuth();
-  console.log(auth);
+    const { loggedIn } = useAuthStatus();
+
   return (
     <>
       <Box sx={{ display: 'flex' }}>
-        {auth.currentUser ? <Navbar /> : <></>}
+        {loggedIn ? <Navbar /> : <></>}
         <Router>
           <Routes>
-            <Route path='/' element={<Dashboard />} />
+            <Route path='/' element={<PrivateRoute />}>
+              <Route path='/' element={<Dashboard />} />
+            </Route>
             <Route path='/sign-in-sign-up' element={<SignInSignUp />} />
           </Routes>
         </Router>
