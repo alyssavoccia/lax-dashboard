df8087c3c2caa856ddb3fbc4f3b7c587d3f62465
Alyssa Voccia <avoccia@hotmail.com>
2022-04-03 17:13:24 -0400

    Added player search to dashboard for admins

---
+++ src/components/DashboardPlayerSearch.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'; 
+import { useSelector } from 'react-redux';
+import { db } from '../firebase.config'; 
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+function DashboardPlayerSearch({ onChange }) {
+  const currentUser = useSelector((state) => state.user.user);
+  const players = [];
+
+  useEffect(() => {
+    const getTeam = async () => {
+      const snapshot = await db.collection(currentUser.team).get();
+      snapshot.docs.map(doc => doc.data().isAdmin ? '' : players.push(doc.data().displayName));
+    }
+    getTeam();
+  }, [currentUser.team]);
+
+  return (
+    <Autocomplete
+      onChange={onChange}
+      disablePortal
+      id="dashboard-player-search"
+      options={players}
+      sx={{ width: 300, backgroundColor: '#FFF' }}
+      renderInput={(params) => <TextField {...params} label="Player" />}
+    />
+  )
+}
+
+export default DashboardPlayerSearch
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
+import DashboardPlayerSearch from '../components/DashboardPlayerSearch';
 
 function Dashboard() {
+  const [selectedPlayer, setSelectedPlayer] = useState({});
+  const currentUser = useSelector((state) => state.user.user);
+
+  const handlePlayerChange = (e, value) => {
+    setSelectedPlayer({
+      chosenPlayer: value
+    });
+  };
+
   return (
     <Box
       component="main"
@@ -17,6 +29,14 @@ function Dashboard() {
       }}
     >
       <Toolbar />
+
+      {currentUser.isAdmin ?
+        <Container maxWidth="lg" sx={{mt: 4, mb: 2}}>
+          <DashboardPlayerSearch onChange={handlePlayerChange} />
+        </Container>
+        
+        : <></>
+      }
 
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         <h1>Lax Dashboard</h1>
