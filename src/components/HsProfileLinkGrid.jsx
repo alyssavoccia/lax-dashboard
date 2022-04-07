d40fbddf6a88743e54003a4840c3fe6c5e25d344
Alyssa Voccia <avoccia@hotmail.com>
2022-04-07 15:58:40 -0400

    Added snackbar on link submission

--- src/components/HsProfileLinkGrid.jsx
+++ src/components/HsProfileLinkGrid.jsx
@@ -3,11 +3,14 @@ import { useSelector } from 'react-redux';
 import { db } from '../firebase.config';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import Grid from '@mui/material/Grid';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 import Title from './Title';
 import HsProfileLink from './HsProfileLink';
 
 function HsProfileLinkGrid() {
   const [userLinkObj, setUserLinkObj] = useState({});
+  const [open, setOpen] = useState(false);
   const currentUser = useSelector((state) => state.user.user);
 
   const {wbLink, threeLink, broadLink, agilityLink} = userLinkObj;
@@ -20,6 +23,11 @@ function HsProfileLinkGrid() {
     }
     getUserLinks();
   }, [currentUser.id, currentUser.team]);
+
+  // Handle snackbar close
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleSubmit = (e) => {
     // Get the input id to know which value is being updated in firebase
@@ -39,7 +47,7 @@ function HsProfileLinkGrid() {
     const docRef = doc(db, currentUser.team, currentUser.id, 'links', currentUser.id);
     updateDoc(docRef, updatedValue);
 
-    alert('Link Submitted!');
+    setOpen(true);
     input.value = '';
   }
   
@@ -61,8 +69,12 @@ function HsProfileLinkGrid() {
       <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
         <HsProfileLink dataTitle="5-10-5 Link" data={agilityLink} handleSubmit={handleSubmit} dataId="agilityLink" />
       </Grid>
+
+      <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right'}} open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>Link successfully submitted!</Alert>
+      </Snackbar>
     </Grid>
   )
 }
 
-export default HsProfileLinkGrid
+export default HsProfileLinkGrid;
