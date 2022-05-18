a34db2e4c89b2d3854b8010955581e2905509e04
Alyssa Voccia <avoccia@hotmail.com>
2022-05-18 07:41:57 -0400

    Updated dashboard player search

--- src/components/DashboardPlayerSearch.jsx
+++ src/components/DashboardPlayerSearch.jsx
@@ -1,20 +1,12 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react'; 
 import { useSelector } from 'react-redux';
-import { db } from '../firebase.config'; 
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 function DashboardPlayerSearch({ onChange }) {
-  const currentUser = useSelector((state) => state.user.user);
+  const currentTeam = useSelector((state) => state.team.team);
   const players = [];
 
-  const getTeam = async () => {
-    const snapshot = await db.collection(currentUser.team).get();
-    snapshot.docs.map(doc => doc.data().isAdmin ? '' : players.push(doc.data().displayName));
-  }
-  
-  getTeam();
+  currentTeam.map(person => !person.isAdmin && players.push(person.displayName));
 
   return (
     <Autocomplete
@@ -28,4 +20,4 @@ function DashboardPlayerSearch({ onChange }) {
   )
 }
 
-export default DashboardPlayerSearch
+export default DashboardPlayerSearch;
